27f78d1e3f248a5bbada3650de520d53977869cb
NeonVulture <NeonVulture@github.com>
2019-10-03 20:44:10 -0400

    Mergesort Implementation

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,42 +1,66 @@
 #include <iostream>
 #include <climits>
+#include <cmath>
 
 using namespace std;
 
 void Merge(int *A, int p, int q, int r) {
   int n1 = q - p + 1;
   int n2 = r - q;
-  int L[n1], R[n2];
+  int  *L[n1], *R[n2];
 
   for(int i = 0; i < n1; i++) {
-    L[i] = A[p + i - 1];
+    //L[i] = A[p + i];
+    L[i] = A[p+ i - 1];
   }
   for(int j = 0; j < n2; j++) {
+    //R[j] = A[q + j + 1];
     R[j] = A[q + j];
   }
-  L[n1 + 1] = INT_MIN;
-  R[n2 + 1] = INT_MIN;
+  // L[n1 + 1] = INT_MIN;
+  //R[n2 + 1] = INT_MIN;
+  //L[n1] = INT_MIN;
+  //R[n2] = INT_MIN;
+
   int i = 0;
   int j = 0;
 
   for(int k = p; p < r; k++) {
     if (L[k] <= R[j]) {
       A[k] = L[i];
-      i = i + 1;
+      i++;
     } else {
       A[k] = R[j];
-      j = j + 1; 
+      j++; 
     }
   }
+
+
+ /* for(int k = p; p < r; k++) {
+      if (i > n1) {
+          A[k] = R[j];
+          j++;
+      } else if (j > n2) {
+          A[k] = L[i];
+          i++;
+      } else if (L[i] <= R[j]) {
+          A[k] = L[j];
+          i++;
+      } else {
+          A[k] = R[j];
+          j++;
+      }
+  }*/
 }
 
 void MergeSort(int *A, int p, int r) {
   if (p < r) {
-    int result = (p+r)/2;
-    int q = floor(q); 
-    MergeSort(*A, p, q);
-    MergeSort(*A, q + 1, r);
-    Merge(*A, p, q, r);
+    //int result = (p+r)/2;
+    //int q = floor(q);
+    int q = p + (r-p)/2; 
+    MergeSort(A, p, q);
+    MergeSort(A, q + 1, r);
+    Merge(A, p, q, r);
   }
 }
 
@@ -50,7 +74,8 @@ void printArray(int *A, int size)
 int main() {
   int A[] = {5,2,4,7,1,3,2,6};
   int ArraySize = sizeof(A)/sizeof(A[0]);
-  printArray(*A, ArraySize);
-  MergeSort(*A, 0, ArraySize - 1)
-  printArray(*A, ArraySize);
+  printArray(A, ArraySize);
+  MergeSort(A, 0, ArraySize - 1);
+  printArray(A, ArraySize);
+  return 0;
 }
