015ea9e92597ba3b89282f332add5baf832cb8a8
NeonVulture <47514287+NeonVulture@users.noreply.github.com>
2019-11-25 23:20:16 -0500

    Update Heapsort.cpp

--- Heapsort.cpp
+++ Heapsort.cpp
@@ -1,1 +1,22 @@
+void MAXHEAPIFY(int *A, int i, int n) {
+     int largest, l , r;
+     l = 2 * i; // left child
+     r = (2 * i) + 1; // right child
+     if (l <= n && A[l - 1] > A[i - 1]) {
+          largest = l;
+     }
+     else {
+          largest = i;
+     }
+     if (r <= n && A[r - 1] > A[largest - 1]) { 
+          largest = i;
+     }
+     if (largest != i) {
+          swap(A[i-1], A[largest - 1]);
+          MAXHEAPIFY(A, largest, n);
+     }
+}
 
+void BUILDMAXHEAP() {
+     
+}
