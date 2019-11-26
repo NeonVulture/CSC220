6c918bbc8011ed672977bba3445b1417943e1cbf
NeonVulture <47514287+NeonVulture@users.noreply.github.com>
2019-11-26 14:27:19 -0500

    Update Heapsort.cpp

--- Heapsort.cpp
+++ Heapsort.cpp
@@ -1,3 +1,15 @@
+int getParent(int i) {
+     return i/2;
+}
+
+int getLeftChild() {
+    return (2*i+1);
+}
+
+int getRightChild() {
+     return (2*i+2);
+}
+
 void MAXHEAPIFY(int *A, int i, int n) {
      int largest, l , r;
      l = 2 * i; // left child
@@ -9,7 +21,7 @@ void MAXHEAPIFY(int *A, int i, int n) {
           largest = i;
      }
      if (r <= n && A[r - 1] > A[largest - 1]) { 
-          largest = i;
+          largest = r;
      }
      if (largest != i) {
           swap(A[i-1], A[largest - 1]);
@@ -17,6 +29,16 @@ void MAXHEAPIFY(int *A, int i, int n) {
      }
 }
 
-void BUILDMAXHEAP() {
-     
+void BUILDMAXHEAP(int *A, int n) {
+      
+}
+
+
+
+void Heapsort(int *A, int n) {
+     BUILDMAXHEAP(A,n);
+     for(int i = n/2; i >= 1 ; i--) { // for i <- length[A] down to 2
+          swap(A[0],A[i-1]);
+          //Something
+          MAXHEAPIFY(A,0,n);
 }
