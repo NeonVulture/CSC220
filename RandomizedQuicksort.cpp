167940e05d62557394c90eab96e88310a9c32ac6
NeonVulture <47514287+NeonVulture@users.noreply.github.com>
2019-11-26 12:21:49 -0500

    Update RandomizedQuicksort.cpp

--- RandomizedQuicksort.cpp
+++ RandomizedQuicksort.cpp
@@ -1,1 +1,64 @@
+#include <iostream>
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
 
+using namespace std;
+using namespace std::chrono;
+
+srand(time(0));
+int RandomPartition (int *A, int p, int r) {
+    int x = A[r]; // Set x equal to the pivot located at A[r] (i.e the last element in A)
+    int i = p + (rand % r - p + 1); // random pivot index i (double check!!!) 
+    for (int j = p; j < r; j++) {
+        if (A[j] <= x) { // if element at A[j] is <= the pivot
+            swap(A[j],A[i]); // Exchange elements A[j] and A[i]
+            i++; // increase the pivot index by 1
+        }
+    }
+    // Executed after the for loop terminates
+    swap(A[i],A[r]); // swap element at pivot index i with the pivot
+    return i;
+}
+
+void RandomizedQuickSort(int *A, int p, int r) {
+    if (p < r) { // Condition needed to prevent infinite recursion
+        int q = RandomPartition(A,p,r); // partition index 
+        QuickSort(A,p,q-1); // 
+        QuickSort(A,q+1,r);
+    }
+}
+
+void printArray(int *A, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << A[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    int A[n];     //create an array with given number of elements
+    srand(time(0));
+    cout << "Enter elements:" << endl;
+    for(int i = 0; i < n; i++) {
+      A[i] = rand() % 1000;
+    }
+    //int A[] = {2,8,10,9,7,13,17,1,3,5,6,4};
+    int arraySize = sizeof(A)/sizeof(A[0]);
+    cout << "Given Array \n \n";
+    printArray(A,arraySize);
+    cout << "\nSorted Array \n \n";
+    auto start = high_resolution_clock::now();
+    QuickSort(A,0,arraySize-1); 
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start); 
+    for (int i = 0; i < arraySize ; i++) {
+        cout << A[i] << " ";
+    }
+    cout << "\n";
+    cout << "Time taken by RandomizedQuicksort " << duration.count()  << " microseconds." << endl;
+    return 0;
+}
