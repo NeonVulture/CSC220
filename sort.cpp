2736e609ce7d775b349a735699ff246c88699c4f
NeonVulture <47514287+NeonVulture@users.noreply.github.com>
2019-11-27 09:26:13 -0500

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -2,3 +2,37 @@
 #include <cstdlib> // For srand() and rand()
 #include <ctime> 
 #include <chrono> // For calculating run-time
+
+using namespace std;
+using namespace std::chrono;
+
+int main() {
+  cout << "\t\t\t\t Welcome!" << endl;
+  cout << "\t\t\t====================================" << endl;
+  cout << "\t\t\t 1 - Insertion_Sort" << endl;
+  cout << "\t\t\t 2 - Merge_Sort" << endl;
+  cout << "\t\t\t 3 - Quicksort" << endl;
+  cout << "\t\t\t 4 - Randomized Quicksort" << endl;
+  cout << "\t\t\t 5 - Heapsort" << endl;
+  
+  int choice = 0;
+  switch(choice) {
+    case 1: // Insertion
+      break:
+    case 2: //Merge
+      break;
+    case 3: // Quicksort
+      break;
+    case 4: // Randomized
+      break;
+    case 5: // Heapsort
+      break;
+    default:
+      cout << "Invaild choice!" << endl;
+      break;
+  }
+  return 0;
+}
+
+/******************************************INSERTION_SORT*************************************/
+/*********************************************************************************************/
