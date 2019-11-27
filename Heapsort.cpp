66560373abce56b8411e808a9e15ee85d7e2392f
NeonVulture <47514287+NeonVulture@users.noreply.github.com>
2019-11-26 20:11:02 -0500

    Update Heapsort.cpp

--- Heapsort.cpp
+++ Heapsort.cpp
@@ -66,7 +66,8 @@ int main() {
      int *A, n;
      cout << "Enter the number of elements: ";
      cin >> n;
-     A = (int*)malloc(sizeof(int)*n);
+     //A = (int*)malloc(sizeof(int)*n);
+     A[n];
      srand(time(0));
      for (int i = 0; i < n; i++) {
           A[i] = rand() % 100;
