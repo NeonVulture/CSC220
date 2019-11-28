67586c0368eb27cf0c3cb7d58430dc8bee899842
NeonVulture <NeonVulture@github.com>
2019-11-27 20:09:59 -0500

    Implemented all algorithms and working

--- sort.cpp
+++ sort.cpp
@@ -22,7 +22,6 @@ void InsertionSort(int *A,int n) {
 }
 /***************************************END_OF_INSERTION_SORT********************************/
 
-
 /***************************************MERGESORT********************************************/
 void Merge(int A[], int p, int q, int r) {
   int n1 = q - p + 1;
@@ -131,6 +130,50 @@ void Heapsort(int *A, int n) {
 }
 /***************************************END_OF_HEAPSORT**************************************/
 
+/******************************************QUICKSORT*****************************************/
+int Partition (int *A, int p, int r) {
+    int x = A[r]; // Set x equal to the pivot located at A[r] (i.e the last element in A)
+    int i = p; // pivot index i 
+    for (int j = p; j < r; j++) {
+        if (A[j] <= x) { // if element at A[j] is <= the pivot
+            swap(A[j],A[i]); // Exchange elements A[j] and A[i]
+            i++; // increase the pivot index by 1
+        }
+    }
+    // Executed after the for loop terminates
+    swap(A[i],A[r]); // swap element at pivot index i with the pivot
+    return i;
+}
+
+void Quicksort(int *A, int p, int r) {
+    if (p < r) { // Condition needed to prevent infinite recursion
+        int q = Partition(A,p,r); // partition index 
+        Quicksort(A,p,q-1); // 
+        Quicksort(A,q+1,r);
+    }
+}
+
+/***************************************END_OF_QUICKSORT*************************************/
+
+/**************************************RANDOMIZED_QUICKSORT**********************************/
+//NOTE: THIS ALGORITHM MAKES USE OF THE PARTITION FUNCTION USED IN QUICKSORT
+
+int RandomizedParition (int *A, int p, int r) {
+    srand(time(0));
+    int i = p + (rand() % (r-p+1));
+    swap(A[r],A[i]);
+    return Partition(A,p,r);
+}
+
+void RandomizedQuicksort(int *A, int p, int r) {
+    if (p < r) { // Condition needed to prevent infinite recursion
+        int q = RandomizedParition(A,p,r); // partition index 
+        RandomizedQuicksort(A,p,q-1); // 
+        RandomizedQuicksort(A,q+1,r);
+    }
+}
+/***********************************END_OF_RANDOMIZED_QUICKSORT******************************/
+
 /******************************************PrintArray****************************************/
 void printArray(int *A, int size) {
    for(int i = 0; i < size; i++)
@@ -214,15 +257,40 @@ int main() {
       cout << "\nTime taken by Heapsort: " << time_span.count() << " microseconds." << endl; 
       break;
     case 4: // Quicksort
-      //int n;
       cout << "Enter the number of elements: ";
-      //cin >> n;
-      //int A[n];
+      cin >> n;
+      A = (int*)malloc(sizeof(int)*n);
+      srand(time(0));
+      for (int i = 0 ; i < n; i++) {
+        A[i] = rand() % 1000;
+      }
+      cout << "Given Array: " << endl;
+      printArray(A,n);
+      start = high_resolution_clock::now();
+      Quicksort(A,0,n-1);
+      stop = high_resolution_clock::now();
+      time_span = duration_cast<microseconds>(stop - start);
+      cout << "Sorted Array via Quicksort: " << endl;
+      printArray(A,n);
+      cout << "\nTime taken by Quicksort: " << time_span.count() << " microseconds." << endl;
       break;
     case 5: // Random
-      //int n, *A;
       cout << "Enter the number of elements: ";
-      //A = (int*)malloc(sizeof(int)*n);
+      cin >> n;
+      A = (int*)malloc(sizeof(int)*n);
+      srand(time(0));
+      for (int i = 0 ; i < n; i++) {
+        A[i] = rand() % 1000;
+      }
+      cout << "Given Array: " << endl;
+      printArray(A,n);
+      start = high_resolution_clock::now();
+      RandomizedQuicksort(A,0,n-1);
+      stop = high_resolution_clock::now();
+      time_span = duration_cast<microseconds>(stop - start);
+      cout << "Sorted Array via Randomized Quicksort: " << endl;
+      printArray(A, n);
+      cout << "\nTime taken by Randomized Quicksort: " << time_span.count() << " microseconds." << endl;
       break; 
     default:
       cout << "Invaild choice!" << endl;
