827cd669c3113e01e817679c1270fa2406a71a80
NeonVulture <47514287+NeonVulture@users.noreply.github.com>
2019-12-09 19:12:23 -0500

    Update sortAverages.cpp

--- sortAverages.cpp
+++ sortAverages.cpp
@@ -1,3 +1,6 @@
+//FILE: sortAverages.cpp
+//AUTHOR: ANTHONY RAMOS
+
 #include <iostream> // For inputing and outputing to the console
 #include <cstdlib> // For srand() and rand()
 #include <ctime> // For implementing srand()
